3f4d348d30be59ca5bd7c3a784d7fe5d31c4bcff
cheeza42 <alonalba@fake.com>
2025-11-24 13:20:36 +0200

    adding check's validator

--- src/machine_model.py
+++ src/machine_model.py
@@ -1,11 +1,14 @@
-# Imports from pydantic provide core functionality for data validation models,
-# including base model definition, IP address validation, and custom field validators.
+'''
+ Imports from 'pydantic' provide core functionality for data validation models,
+ including base model definition, IP address validation, and custom field validators.
+'''
+
 from pydantic import BaseModel, IPvAnyAddress, field_validator
 
 # Import from typing to restrict field values to specific literal options (like enums)
 from typing import Literal, Optional
 
-# VMInstance defines the structure and validation rules for a virtual machine instance
+# VMInstance defines the structure and validation rules for the VM's instance. 
 class VMInstance(BaseModel):
     # Name of the VM (must be a non-empty string)
     name: str
@@ -16,10 +19,12 @@ class VMInstance(BaseModel):
     # Operating system description (can contain a version but must start with a known OS name)
     os: str
 
-    # Machine status (must be either "UP" or "DOWN")
     status: Literal["UP", "DOWN"]
     
+    # URL check   
     check: Literal["ping", "http"] = "ping"
+    url: Optional[str] = None
+
     # Validator to ensure the 'name' field is not empty or just whitespace
     @field_validator("name")
     @classmethod
@@ -50,4 +55,18 @@ class VMInstance(BaseModel):
     def status_must_be_valid(cls, v):
         if v not in {"UP", "DOWN"}:
             raise ValueError("Status must be either 'UP' or 'DOWN'")
+        return v
+   
+    #Validator to ensure the 'check' field is valid
+    @field_validator("url")
+    @classmethod
+    def url_required_if_http(cls, v, info):
+        check_value = info.data.get("check")
+
+        if check_value == "http" and not v:
+            raise ValueError("URL is required when check='http'")
+
+        if check_value == "ping" and v is not None:
+            raise ValueError("URL must not be provided when check='ping'")
+
         return v
